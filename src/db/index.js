cb3114f9f25b164b99af0ef5b8485580e0e4b5e3
maheswar behera <bmaheswar439@gmail.com>
2024-08-18 16:14:48 +0530

    postman added

--- src/db/index.js
+++ src/db/index.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 import {DB_NAME} from "../constants.js";
 
 const connectDb = async () => {
+    console.log(process.env.MONGODB_URI)
     try {
         const connectionInstance = await  mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
         // console.log(connectionInstance)
